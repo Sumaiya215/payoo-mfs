b8547cf0b1135712cc37654838b881d75af3cd95
Sumaiya215 <sumaiyacse15>
2024-09-21 12:05:57 -0700

    login page completed

--- js/login.js
+++ js/login.js
@@ -5,9 +5,19 @@ addEventListener('click',function(event){
 
 // step-2: prevent default behavior (prevent reloading browser)
     event.preventDefault();
-    console.log("Login Button clicked");
+    // console.log("Login Button clicked");
 
-// step-3: get the phone number
+// step-3: get the phone number and pin number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log( phoneNumber,pinNumber);
+
+// step-4: validate phone and pin
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log("you are logged in");
+        window.location.href = '/home.html';
+    }
+    else{
+        alert("wrong phone number or pin");
+    }
 })
